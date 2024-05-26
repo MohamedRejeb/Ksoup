4897af3790f70f3843b5ff58ae17b9813cf1748f
Mohamed Rejeb <41842296+MohamedRejeb@users.noreply.github.com>
2024-05-26 19:41:26 +0200

    Add gradle toolchains resolver (#49)
    * Update README.md
    
    * Add gradle toolchains resolver

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -17,6 +17,10 @@ dependencyResolutionManagement {
     }
 }
 
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.4.0"
+}
+
 include(
     ":ksoup-html",
     ":ksoup-entities",
